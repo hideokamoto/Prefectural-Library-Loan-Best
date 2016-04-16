ab33ff0db436b62ccc743558123fba5b411d0561
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-16 17:53:05 +0900

    create api script

--- deploy/csv2json.js
+++ deploy/csv2json.js
@@ -4,19 +4,23 @@ var sjis = new Iconv('SJIS', 'UTF-8//TRANSLIT//IGNORE');
 var csv = require("comma-separated-values");
 var stdout = process.stdout;
 
-fs.readFile("../H26-Kinan-library-lending-best-100(Children's book).csv", function (err, text) {
-  text = sjis.convert(text).toString();
-  var result = new csv(text, { header: true }).parse();
-  var data = [];
-  for (var i=0; i<result.length; i++) {
-    var line = result[i];
-    var row = {};
-    row.rank = line['順位'];
-	row.num = line['貸出数'];
-	row.isbn = line['ＩＳＢＮ'];
-	row.category = line['分類'];
-	row.book = line['書誌情報'];
-    data.push(row);
-  }
-  stdout.write(JSON.stringify(data));
-});
+if ( process.argv[2] ) {
+	var filename = process.argv[2];
+
+	fs.readFile( filename, function (err, text) {
+	  text = sjis.convert(text).toString();
+	  var result = new csv(text, { header: true }).parse();
+	  var data = [];
+	  for (var i=0; i<result.length; i++) {
+	    var line = result[i];
+	    var row = {};
+	    row.rank = line['順位'];
+		row.num = line['貸出数'];
+		row.isbn = line['ＩＳＢＮ'];
+		row.category = line['分類'];
+		row.book = line['書誌情報'];
+	    data.push(row);
+	  }
+	  stdout.write(JSON.stringify(data));
+	});
+}
--- deploy/prepare.sh
+++ deploy/prepare.sh
@@ -6,4 +6,10 @@ cd deploy/
 
 npm install
 mkdir v1
-node csv2json.js > v1/wapuu.json
+mkdir v1/kinan
+mkdir v1/main
+
+node csv2json.js "../H26-Kinan-library-lending-best-100(Children's book).csv" > v1/kinan/children.json
+node csv2json.js "../H26-Kinan-library-lending-best-100(General books).csv" > v1/kinan/general.json
+node csv2json.js "../H26-Main-library-lending-best-100(Children's book).csv" > v1/main/children.json
+node csv2json.js "../H26-Main-library-lending-best-100(General books).csv" > v1/main/general.json
